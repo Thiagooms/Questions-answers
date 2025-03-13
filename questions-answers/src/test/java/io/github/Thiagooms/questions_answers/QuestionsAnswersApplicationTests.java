3c12d396d0be753863729f38ce857b1e935287ef
Thiago Monteiro <thiagomonteiroedc@gmail.com>
2025-03-13 19:53:36 -0300

    [ADD] Estrutura Spring

---
+++ questions-answers/src/test/java/io/github/Thiagooms/questions_answers/QuestionsAnswersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Thiagooms.questions_answers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsAnswersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
